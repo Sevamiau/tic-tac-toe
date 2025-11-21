7ef87d1e77b35e892ed723b8120884d03a175bae
seva <sevakunjadas.bms@gmail.com>
2025-11-21 18:43:14 -0300

    createPlayer readyy

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+function createGameBoard() {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = () => board;
+
+    function placeMark(index, playerMark){
+        if (board[index] === ""){
+            board[index] = playerMark;
+            return true;
+        } else {
+            return false
+        }
+    };
+
+    return {
+        getBoard: getBoard,
+        placeMark: placeMark
+    };
+}
+
+
+function createPlayer(name, mark) {
+    
+
+    return {
+        name,
+        mark
+    }
+};
+
+
+
+const myBoard = createGameBoard();
+console.log('Initial Board: ', myBoard.getBoard());
+myBoard.placeMark(0, "X")
+myBoard.placeMark(4, "O")
+console.log(myBoard.getBoard())
